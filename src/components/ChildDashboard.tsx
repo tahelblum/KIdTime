8c2be99b5d54d2aabee7cb9d00f2356f5a517a68
tahelblum <tahelblum@gmail.com>
2026-02-13 10:57:27 +0200

    Update ChildDashboard.tsx

--- src/components/ChildDashboard.tsx
+++ src/components/ChildDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ChildDashboard = () => {
     const [childName, setChildName] = useState('');
